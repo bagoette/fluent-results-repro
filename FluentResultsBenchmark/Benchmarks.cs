8f05f7f3a07e195e3fc640ea7f5e24cf698797b5
Barney Goette <barney.a.goette@loram.com>
2023-08-25 10:22:51 -0500

    init

---
+++ FluentResultsBenchmark/Benchmarks.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentResultsBenchmark;
+
+using BenchmarkDotNet.Attributes;
+using FluentResults;
+
+public class Benchmarks
+{
+    [Benchmark]
+    public void TestWith_FluentResults()
+    {
+        var result = GetRandomEven();
+
+        if (result.IsFailed)
+        {
+            Console.WriteLine("Failed");
+        }
+
+        Console.WriteLine($"Random even: {result.Value}");
+    }
+
+    private static Result<long> GetRandomEven()
+    {
+        var random = Random.Shared.NextInt64();
+
+        return random % 2 == 0
+            ? random
+            : Result.Fail("Not even");
+    }
+}
---
+++ FluentResultsBenchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using FluentResultsBenchmark;
+
+BenchmarkRunner.Run<Benchmarks>();
